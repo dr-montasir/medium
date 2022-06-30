16ed0d79113fb98e002438c074630f45583ef874
Dr. Montasir <montasir1400@gmail.com>
2022-06-30 22:50:20 +0300

    Header | signin

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,7 +2,7 @@ import Link from "next/link"
 
 function Header() {
   return (
-    <header>
+    <header className="flex justify-between p-5 max-w-7xl mx-auto">
       <div className="flex items-center space-x-5">
         <Link href="/">
           <img className="w-44 object-contain cursor-pointer" src="/img/logo.png" alt="medium" />
@@ -13,7 +13,11 @@ function Header() {
           <h1 className="bg-green-600 text-white px-4 py-1 rounded-full">Follow</h1>
         </div>
       </div>
-      <div></div>
+
+      <div className="flex items-center space-x-5 text-green-600">
+        <h3>Sign In</h3>
+        <h3 className="border px-4 py-1 rounded-full border-green-600">Get Started</h3>
+      </div>
     </header>
   )
 }
